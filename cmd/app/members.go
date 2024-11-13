d2f63798fc7433fddbe8590f01c63f72009c038e
ak2kay <yandli@hotmail.com>
2024-11-13 19:31:04 +0800

    fix(members): old version gitlab don't support /all endpoints

--- cmd/app/members.go
+++ cmd/app/members.go
@@ -23,34 +23,42 @@ type projectMemberService struct {
 
 /* projectMembersHandler returns all members of the current Gitlab project */
 func (a projectMemberService) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-
-	projectMemberOptions := gitlab.ListProjectMembersOptions{
+	projectMemberOptions := &gitlab.ListProjectMembersOptions{
 		ListOptions: gitlab.ListOptions{
 			PerPage: 100,
 		},
 	}
 
-	projectMembers, res, err := a.client.ListAllProjectMembers(a.projectInfo.ProjectId, &projectMemberOptions)
+	var allProjectMembers []*gitlab.ProjectMember
+	for {
+		projectMembers, res, err := a.client.ListAllProjectMembers(a.projectInfo.ProjectId, projectMemberOptions)
+		if err != nil {
+			handleError(w, err, "Could not retrieve project members", http.StatusInternalServerError)
+			return
+		}
 
-	if err != nil {
-		handleError(w, err, "Could not retrieve project members", http.StatusInternalServerError)
-		return
-	}
+		if res.StatusCode >= 300 {
+			handleError(w, GenericError{r.URL.Path}, "Could not retrieve project members", res.StatusCode)
+			return
+		}
 
-	if res.StatusCode >= 300 {
-		handleError(w, GenericError{r.URL.Path}, "Could not retrieve project members", res.StatusCode)
-		return
+		allProjectMembers = append(allProjectMembers, projectMembers...)
+
+		if res.NextPage == 0 {
+			break
+		}
+
+		projectMemberOptions.Page = res.NextPage
 	}
 
 	w.WriteHeader(http.StatusOK)
 
 	response := ProjectMembersResponse{
 		SuccessResponse: SuccessResponse{Message: "Project members retrieved"},
-		ProjectMembers:  projectMembers,
+		ProjectMembers:  allProjectMembers,
 	}
 
-	err = json.NewEncoder(w).Encode(response)
-	if err != nil {
+	if err := json.NewEncoder(w).Encode(response); err != nil {
 		handleError(w, err, "Could not encode response", http.StatusInternalServerError)
 	}
 }
